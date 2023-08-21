152ce80594794b675637ce433b58a5f3ea6f1dbc
farneet7 <140754357+farneet7@users.noreply.github.com>
2023-08-21 18:39:29 +0530

    Update CNN_LSTM.py

--- CNN_LSTM.py
+++ CNN_LSTM.py
@@ -55,3 +55,68 @@ Test Accuracy: 63.91608119010925%
     accuracy                           0.64      4290
    macro avg       0.65      0.66      0.65      4290
 weighted avg       0.64      0.64      0.64      4290
+
+
+
+
+
+
+
+# CNN for Melspectrogram
+input_mel = Input(shape=(mel_height, mel_width, 1))  # Adjust shape accordingly
+x = Conv2D(512, (3, 3), activation='relu')(input_mel)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(256, (3, 3), activation='relu')(x)
+x = MaxPooling2D((2, 2))(x)
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.5)(x)  # Dropout layer added here
+cnn_output = Dense(128, activation='relu')(x)
+
+# Bidirectional LSTM for sequential features
+input_seq = Input(shape=(timesteps, features))  # Adjust shape accordingly
+
+y = Bidirectional(LSTM(256, return_sequences=True))(input_seq)
+attention_output = MultiHeadAttention(num_heads=2, key_dim=128)(y, y)
+# Adding residual connection
+y = add([y, attention_output])
+y = Dropout(0.5)(y)
+
+y = Bidirectional(LSTM(256, return_sequences=True))(y)
+attention_output = MultiHeadAttention(num_heads=2, key_dim=128)(y, y)
+# Adding residual connection
+y = add([y, attention_output])
+y = Dropout(0.5)(y)
+
+y = Bidirectional(LSTM(128))(y)
+y = Dropout(0.5)(y)  # Dropout layer added here
+lstm_output = Dense(128, activation='relu')(y)
+
+# Combining both models
+combined = concatenate([cnn_output, lstm_output])
+z = Dense(128, activation='relu')(combined)
+z = Dropout(0.5)(z)  # Dropout layer added here
+z = Dense(num_classes, activation='softmax')(z)  # Adjust num_classes for your data
+
+model = Model(inputs=[input_mel, input_seq], outputs=z)
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+135/135 [==============================] - 5s 30ms/step - loss: 0.9829 - accuracy: 0.6413
+Test Accuracy: 64.1258716583252%
+
+CLASSIFICATION REPORT
+                precision   recall  f1-score   support
+
+           0       0.76      0.77      0.76       688
+           2       0.61      0.54      0.57       688
+           3       0.62      0.55      0.58       688
+           4       0.60      0.57      0.58       686
+           5       0.57      0.62      0.59       612
+           6       0.62      0.73      0.67       692
+           7       0.85      0.83      0.84       236
+
+    accuracy                           0.64      4290
+   macro avg       0.66      0.66      0.66      4290
+weighted avg       0.64      0.64      0.64      4290
