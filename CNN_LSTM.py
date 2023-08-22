883897a552a6a11751fd76aa145b7e199a26d5a9
farneet7 <140754357+farneet7@users.noreply.github.com>
2023-08-22 16:00:13 +0530

    Update CNN_LSTM.py

--- CNN_LSTM.py
+++ CNN_LSTM.py
@@ -195,3 +195,87 @@ CLASSIFICATION REPORT
     accuracy                           0.66      4290
    macro avg       0.67      0.69      0.68      4290
 weighted avg       0.66      0.66      0.66      4290
+
+# <------------------------------------------------ TRIAL 4 ------------------------------------------------------------>
+
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (Input, Dense, LSTM, Bidirectional, 
+                                     Conv2D, MaxPooling2D, Flatten, concatenate, MultiHeadAttention)
+from tensorflow.keras.layers import add
+from tensorflow.keras.layers import Dropout
+
+# CNN for Melspectrogram
+input_mel = Input(shape=(mel_height, mel_width, 1))  # Adjust shape accordingly
+x = Conv2D(512, (3, 3), activation='relu')(input_mel)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(256, (3, 3), activation='relu')(x)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(256, (3, 3), activation='relu')(x)
+x = MaxPooling2D((2, 2))(x)
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.5)(x)  # Dropout layer added here
+cnn_output = Dense(128, activation='relu')(x)
+
+# Bidirectional LSTM for sequential features
+input_seq = Input(shape=(timesteps, features))  # Adjust shape accordingly
+
+y = Bidirectional(LSTM(512, return_sequences=True))(input_seq)
+attention_output = MultiHeadAttention(num_heads=2, key_dim=64)(y, y)
+# Adding residual connection
+y = add([y, attention_output])
+y = Dropout(0.5)(y)
+
+y = Bidirectional(LSTM(256, return_sequences=True))(y)
+attention_output = MultiHeadAttention(num_heads=2, key_dim=64)(y, y)
+# Adding residual connection
+y = add([y, attention_output])
+y = Dropout(0.5)(y)
+
+y = Bidirectional(LSTM(256, return_sequences=True))(input_seq)
+attention_output = MultiHeadAttention(num_heads=2, key_dim=64)(y, y)
+# Adding residual connection
+y = add([y, attention_output])
+y = Dropout(0.5)(y)
+
+y = Bidirectional(LSTM(128))(y)
+y = Dropout(0.5)(y)  # Dropout layer added here
+lstm_output = Dense(128, activation='relu')(y)
+
+# Combining both models
+combined = concatenate([cnn_output, lstm_output])
+z = Dense(128, activation='relu')(combined)
+z = Dropout(0.5)(z)  # Dropout layer added here
+z = Dense(num_classes, activation='softmax')(z)  # Adjust num_classes for your data
+
+model = Model(inputs=[input_mel, input_seq], outputs=z)
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+
+Epoch 16/30
+256/256 [==============================] - ETA: 0s - loss: 0.5846 - accuracy: 0.7809Restoring model weights from the end of the best epoch: 11.
+256/256 [==============================] - 49s 193ms/step - loss: 0.5846 - accuracy: 0.7809 - val_loss: 0.9229 - val_accuracy: 0.6760 - lr: 1.0000e-05
+Epoch 16: early stopping
+
+
+TEST RESULTS
+135/135 [==============================] - 4s 30ms/step - loss: 0.9007 - accuracy: 0.6727
+Test Accuracy: 67.27272868156433%
+
+CLASSIFICATION REPORT
+                precision   recall  f1-score   support
+
+           0       0.75      0.78      0.76       688
+           2       0.64      0.61      0.62       688
+           3       0.65      0.54      0.59       688
+           4       0.66      0.60      0.63       686
+           5       0.65      0.69      0.67       612
+           6       0.65      0.75      0.70       692
+           7       0.73      0.89      0.80       236
+
+    accuracy                           0.67      4290
+   macro avg       0.68      0.69      0.68      4290
+weighted avg       0.67      0.67      0.67      4290
