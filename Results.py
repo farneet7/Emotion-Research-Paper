7a0189ce468a2ebde404f748e4ba04a4bee21274
farneet7 <140754357+farneet7@users.noreply.github.com>
2024-03-20 22:13:44 +0530

    Create Results.py

---
+++ Results.py
@@ -0,0 +1,24 @@
+Final results of Research
+
+1. Speech emotion recognition system - 84.56% Validation accuracy - CNN-BiLSTM - RAVDESS + SAVEE + CREMA-D + TESS
+
+                precision    recall  f1-score   support
+
+       angry       0.88      0.89      0.89      1169
+        calm       0.90      0.95      0.93       105
+     disgust       0.82      0.80      0.81      1165
+        fear       0.82      0.85      0.83      1144
+       happy       0.84      0.82      0.83      1168
+     neutral       0.83      0.84      0.83      1004
+         sad       0.85      0.84      0.84      1144
+   surprised       0.94      0.93      0.94       399
+
+    accuracy                           0.85      7298
+   macro avg       0.86      0.86      0.86      7298
+weighted avg       0.85      0.85      0.85      7298
+
+
+2. Contextual Emotion recognition - Fine-tuned Roberta on Emocontext and Custom data
+
+
+3. Facial Emotion Recognition Model - CNN with Attention model
